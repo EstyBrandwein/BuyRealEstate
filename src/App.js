fa7bd2d498e79e4ca0dbfc7026ddfcc1b77b7a1e
EstyBrandwein <str8546200@gmail.com>
2024-12-08 16:42:12 +0200

    react plot and project service

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Sidebar from './Pages/sideBarPage';
+import MyFields from './Pages/myFeiledPage';
+import EditProfile from './Pages/editProfilePage';
+import Login from './Pages/homePage';
+import MainPage from './Pages/mainPage';
+import DetailsPage from './Pages/myFeiledPage';
+import PaymentPage from './Pages/paymentsPage';
+import GrossProfitPage from './Pages/profitCalculations';
+import PlotsPage from './Pages/realEstateDetails';
 
 function App() {
+  const handleLogout = () => {
+    // פונקציה להתנתקות
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          {/* <Route path="/login" element={<Login />} /> */}
+          <Route path="/my-fields" element={<PlotsPage />} />
+          {/* <Route path="/edit-profile" element={<EditProfile />} /> */}
+          <Route path="/" element={<MainPage />} />
+          <Route path="/" element={<MainPage />} />
+        <Route path="/gross-profit" element={<GrossProfitPage />} />
+        <Route path="/payments" element={<PaymentPage />} />
+          <Route path="/my-fields" render={() => (
+          <div>
+            <Sidebar username="User123" email="user123@example.com" onLogout={handleLogout} />
+            <MyFields />
+          </div>
+        )} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
