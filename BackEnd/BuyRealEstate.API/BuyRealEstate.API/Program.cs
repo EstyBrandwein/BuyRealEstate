0ea49ff479270dc4dad6f606deb92b289a5e30ec
EstyBrandwein <str8546200@gmail.com>
2025-01-07 11:26:46 +0200

    add from the git ignore

---
+++ BackEnd/BuyRealEstate.API/BuyRealEstate.API/Program.cs
@@ -0,0 +1,68 @@
+using BuyRealEstate.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using BuyRealEstate.Domain.Models;
+using BuyRealEstate.Core.Interfaces;
+using BuyRealEstate.Core.Services;
+using BuyRealEstate.Domain.Interfaces;
+using BuyRealEstate.Core.DTos;
+using BuyRealEstate.Core.DTOs;
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddAutoMapper(cfg =>
+{
+    // éöéøú îéôåééí áéï äîåãìéí åä-DTOs ùìê
+    cfg.CreateMap<DevelopmentStatus, DevelopmentStatusDTO>();
+    cfg.CreateMap<User, UserDto>();
+    cfg.CreateMap<LegalStatus, LegalStatusDTO>();
+    cfg.CreateMap<Image, ImageDTO>();
+    cfg.CreateMap<Document, DocumentDTO>();
+    // îéôåé ùì ôøåé÷è
+    cfg.CreateMap<Project, ProjectDTO>()
+        .ForMember(dest => dest.ProjectManager, opt => opt.MapFrom(src => src.ProjectManager))
+        .ForMember(dest => dest.DeveloperStatus, opt => opt.MapFrom(src => src.DeveloperStatus))
+        .ForMember(dest => dest.LegalStatus, opt => opt.MapFrom(src => src.LegalStatus))
+        .ForMember(dest => dest.Images, opt => opt.MapFrom(src => src.Images))
+        .ForMember(dest => dest.Documents, opt => opt.MapFrom(src => src.Documents));
+    // îéôåé ùì Plot
+    cfg.CreateMap<Plot, PlotDTO>()
+        .ForMember(dest => dest.Project, opt => opt.MapFrom(src => src.Project));
+}, AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IPlotRepository, PlotRepository>();
+builder.Services.AddScoped<IPlotService, PlotService>();
+builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
+
+builder.Services.AddScoped<IProjectService, ProjectService>();
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conection_String")));
+
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
+app.MapRazorPages();
+app.Run();
+
---
+++ BackEnd/BuyRealEstate.API/BuyRealEstate.Core/DTos/ProjectDTO.cs
@@ -0,0 +1,28 @@
+﻿
+using BuyRealEstate.Core.DTos;
+using BuyRealEstate.Domain.Models;
+
+namespace BuyRealEstate.Core.DTOs
+{
+    public class ProjectDTO
+    {
+        public int ID { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectAddress { get; set; }
+        public UserDto ProjectManager { get; set; }   // Assuming Users is a related class
+        public double GrossProfit { get; set; }
+        public string LinkToLead2 { get; set; }
+        public bool ProjectType { get; set; } // Clarify if this represents a specific type or a flag
+        public ICollection<DocumentDTO> Documents { get; set; } // Changed to ICollection for better EF Core compatibility
+        public ICollection<ImageDTO> Images { get; set; }
+        public ICollection<PlotDTO> Plots { get; set; }
+        public DevelopmentStatusDTO DeveloperStatus { get; set; }
+        public LegalStatusDTO LegalStatus { get; set; }
+        public int LegalStatusId { get; set; }
+        // Changed to ICollection for better EF Core compatibility
+        public ICollection<RelationshipPaymentsProjects> PaymentProject { get; set; }
+        public int DeveloperStatusID { get; set; }
+        public int ProjectManagerID { get; set; }
+
+    }
+}
